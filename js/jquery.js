af8f1f36bf069b51396006bfa4b1c1cb425be164
LukaTchubabria <lukatchubabria@gmail.com>
2021-05-08 05:41:48 +0400

    Jquery validation

--- js/jquery.js
+++ js/jquery.js
@@ -1,12 +1,17 @@
 //Create bank
+
 $("#teacher_bank").on("click", function() {
-    if ($("#teacher_form").hasClass("disnone")){
-        $('#teacher_form').removeClass('disnone');
+    if ($("form").hasClass("disnone")){
+        $('form').removeClass('disnone');
     } else {
-        $('#teacher_form').addClass('disnone');
+        $('form').addClass('disnone');
     }
 });
+
+
+
 // Collapse-Additional questions
+
 $("#qeust_checkbox").on("click", function() {
     if ($("#quest_collapse").hasClass("disnone")){
         $('#quest_collapse').removeClass('disnone');
@@ -14,6 +19,7 @@ $("#qeust_checkbox").on("click", function() {
         $('#quest_collapse').addClass('disnone');
     }
 });
+
 
 
 // Completion Date
@@ -27,26 +33,12 @@ $("form").on('click', '.working_now', function() {
     }
 });
 
-//date validation
-
-// start_day.max = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0];
-// end_day.max = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0];
-
-// $(".start_day").on('change', function() {
-//     if (start_day.value)
-//     end_day.min = start_day.value;
-// });
-// $(".end_day").on('change', function() {
-//     if (end_day.value)
-//     start_day.max = end_day.value;
-// });
-
 
 
 // Add additional questions
 
 $('.add_questions').on('click', function() {
-    $('#teacher_form').append('\
+    $('form').append('\
     <div class="add_additional_question">\
         <input class="main_question" type="text" name="name" placeholder="კითვის მთავარი ველი (text)">\
         <input class="answer_type" type="text"  name="name" placeholder="პასუხის ტიპის არჩევა">\
@@ -57,27 +49,47 @@ $('.add_questions').on('click', function() {
     </div>')
 });
 
+
+
 // Add answer
 
 $("form").on('click', '.add_answer', function() {
     $(this).before('<input class="answer" type="text" name="name" placeholder="პირველი პასუხის ველი"><br>')
 });
 
+
+
 // Add work Experience
+
+var work_experience = 1
 $('.add_work_experience').on('click', function() {
+    //if you want to add a block above, write "$('#mob_num').after"
     $('.add_work_experience').before('\
+    <div id="work-exp-cont-'+ (work_experience) +'" class="content-box">\
+        <label for="Work-experience">სამუშაო გამოცდილება</label>\
+        <input type="text" name="work-exp-'+ (work_experience) +'" id="work-exp-'+ (work_experience) +'" class="form_input workexp-valid">\
+    </div>\
+    <div id="org-name_cont'+ (work_experience) +'" class="content-box">\
+        <label for="Org-name">ორგანიზაციის სახელი</label>\
+        <input type="text" name="org-name-'+ (work_experience) +'" id="org-name'+ (work_experience) +'" class="form_input workexp-valid">\
+    </div>\
+    <div id="position-cont'+ (work_experience) +'" class="content-box">\
+        <label for="pos">თქვენი პოზიცია</label>\
+        <input type="text" name="position-'+ (work_experience) +'" id="position-'+ (work_experience) +'" class="form_input workexp-valid">\
+    </div>\
     <div class="job_checkbox_label">\
         <label>\
             <input class="working_now checkbox" type="checkbox">\
             მოცემულ მომეტში ვმუშაობ აღნიშნულ ორგანიზაციაში\
             <div class="content-box">\
                 <label for="start">დაწყების თარიღი:</label>\
-                <input type="date" id="start_day" class="form_input" name="start_date" min="2018-01-01" required>\
+                <input type="date" id="start_day_'+ (work_experience) +'" class="form_input date-valid" name="start_day'+ (work_experience) +'">\
             </div>\
             <div class="end_date content-box">\
                 <label for="end">დასრულების თარიღი:</label>\
-                <input type="date" id="end_day" class="form_input" name="end_date" min="2018-01-01" required>\
+                <input type="date" id="end_day_'+ (work_experience) +'" class="form_input date-valid" name="end_day'+ (work_experience) +'">\
             </div>\
         </label>\
-    </div>')
+    </div>');
+    work_experience++;
 });
